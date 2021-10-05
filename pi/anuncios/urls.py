7e8f36db80ebd4ec0adb4464df221c6f7c5cf659
alexsoares4a <73480812+alexsoares4a@users.noreply.github.com>
2021-10-04 22:07:34 -0300

    Páginas de listagem e detalhes dos produtos e serviços

--- pi/anuncios/urls.py
+++ pi/anuncios/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('produtos/', views.produtos, name = 'produtos'),
     path('servicos/', views.servicos, name = 'servicos'),
+    path('lista_produtos/', views.listaProdutos, name = 'lista_produtos'),
+    path('lista_servicos/', views.listaServicos, name = 'lista_servicos'),
+    path('detalhes_produto/', views.detalhesProduto, name = 'detalhes_produto'),      
+    path('detalhes_servico/', views.detalhesServico, name = 'detalhes_servico'),  
 ]
--- pi/anuncios/views.py
+++ pi/anuncios/views.py
@@ -8,3 +8,16 @@ def produtos(request):
 
 def servicos(request):
     return render(request, 'anuncios/servicos.html')
+
+def listaProdutos(request):
+    return render(request, 'anuncios/lista_produtos.html')
+
+def listaServicos(request):
+    return render(request, 'anuncios/lista_servicos.html')
+
+def detalhesProduto(request):
+    return render(request, 'anuncios/detalhes_produto.html')
+
+def detalhesServico(request):
+    return render(request, 'anuncios/detalhes_servico.html')
+
